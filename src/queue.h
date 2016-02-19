3bc7b03f2df8d74c9669483e47b701127dae4bad
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2016-02-19 00:43:22 +0000

    Work in progress multi-producers/multi-consumers queue

---
+++ src/queue.h
@@ -0,0 +1,113 @@
+#pragma once
+#include <chrono>
+#include <cstddef>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+
+namespace mongo_smasher {
+  
+//
+// Queue is a concurrent multi-producers multi-readers queue
+//
+// This queue is used to dispatch the produced documents to
+// the writer threads.
+template <class T> class Queue {
+  size_t size_ { 0u };
+  size_t max_size_ { 100000u };
+  size_t waiting_readers_ {0};
+  size_t waiting_writers_ {0};
+  mutable std::mutex main_lock_;
+  std::condition_variable push_guard_;
+  std::condition_variable pop_guard_;
+  std::queue<T> values_;
+
+ public:
+  using duration_t = std::chrono::duration<size_t>;
+  using wait_clock_t = std::chrono::high_resolution_clock;
+  using time_point_t = std::chrono::time_point<wait_clock_t>;
+
+  Queue() = default;
+  Queue(size_t max_size) : max_size_{max_size} {}
+  Queue(Queue&&) = default;
+
+  size_t size() const {
+    std::lock_guard<std::mutex> lock(main_lock_);
+    return size_;
+  }
+
+  // 
+  // Pushes a value to be read on the queue.
+  //
+  // Since the funtion can block if the queue is full or busy,
+  // it returns the time taken to push.
+  duration_t push(T&& value) {
+    std::unique_lock<std::mutex> lock(main_lock_);
+    
+    // If queue is full, we wait
+    duration_t wait_time {0u};
+    if (max_size_ <= size_) {
+      ++waiting_writers_;
+      time_point_t start = wait_clock_t::now();
+      push_guard_.wait(lock);
+      wait_time = std::chrono::duration_cast<duration_t>(wait_clock_t::now() - start);
+      --waiting_writers_;
+    }
+
+    // Add to the queue
+    values_.push(std::move(value));
+    ++size_;
+
+    // Help another potential waiting thread to be unlocked
+    if (waiting_readers_) {
+      lock.unlock();
+      pop_guard_.notify_all();
+    }
+    else if (size_ < max_size_ - 1 && waiting_writers_) {
+      lock.unlock();
+      push_guard_.notify_all();
+    }
+
+    return wait_time;
+  }
+
+  // 
+  // Pops a value on the queue.
+  //
+  // The function blocks if there is nothing to consume. Can take an optional
+  // duration meant to be affected in case of block.
+  T pop(duration_t* block_duration = nullptr) {
+    std::unique_lock<std::mutex> lock(main_lock_);
+
+    // if queue is empty, we wait
+    if (block_duration)
+      *block_duration = duration_t {0u};
+    if (0u == size_) {
+      ++waiting_readers_;
+      time_point_t start = wait_clock_t::now();
+      pop_guard_.wait(lock);
+      if (block_duration)
+        *block_duration = std::chrono::duration_cast<duration_t>(wait_clock_t::now() - start);
+      --waiting_readers_;
+    }
+
+    // Pops the element
+    T value { std::move(values_.front()) };
+    values_.pop();
+    --size_;
+
+    if (0u < size_ && waiting_readers_) {
+      lock.unlock();
+      pop_guard_.notify_all();
+    }
+    else if (waiting_writers_) {
+      lock.unlock();
+      push_guard_.notify_all();
+    }
+
+    return value;
+  }
+};
+
+
+}  // namespace mongo_smasher
--- src/test/queue.cc
+++ src/test/queue.cc
@@ -1,6 +1,106 @@
 #include <catch.hpp>
+#include "../queue.h"
+#include <thread>
+#include <future>
+#include "../logger.h"
 
-TEST_CASE("Simple test","[queue]") {
-  REQUIRE(1 < 2);
+using namespace std;
+using namespace mongo_smasher;
+
+namespace {
+struct Movable {
+  bool valid {true};
+  int const value {0};
+  Movable() = default;
+  Movable(Movable&& other) {
+    other.valid = false;
+  }
+  Movable(Movable const&) = delete;
+  Movable& operator=(Movable&& other) {
+    valid = true;
+    other.valid = false;
+    return *this;
+  }
+  Movable& operator=(Movable const&) = delete;
+  bool is_valid() const { return valid; }
+};
+
+bool run_writers_readers(Queue<Movable>& queue, size_t nb_writers, size_t nb_writes, size_t nb_readers) {
+  // Prepare number of iterations
+  size_t nb_reads = nb_writers * nb_writes / nb_readers;
+  if (0 == nb_reads) {
+    // failure : test would block
+    return false;
+  }
+
+  // Launch writers
+  vector<thread> writers;
+  for (int w=0; w < nb_writers; ++w) {
+    thread writer {[&queue,&nb_writes]() -> void { 
+      for (int i=0; i< nb_writes; ++i) {
+        queue.push(Movable {});
+        //log(log_level::info,"Push one\n");
+      }
+    }};
+    writer.detach();
+    writers.emplace_back(move(writer));
+  }
+
+  vector<future<bool>> futures;
+  vector<thread> readers;
+
+  for (int r=0; r < nb_readers; ++r) {
+    std::promise<bool> read_valid_promise;
+    futures.emplace_back(read_valid_promise.get_future());
+
+    thread reader {[&queue,&nb_reads] (decltype(read_valid_promise)&& result) -> void {
+      bool valid = true;
+      for (int i=0; i< nb_reads; ++i) {
+        Movable m = queue.pop();
+        //log(log_level::info,"Pop one\n");
+        valid = valid && m.is_valid();
+      }
+      result.set_value(valid); 
+    }, std::move(read_valid_promise) };
+    reader.detach();
+    readers.emplace_back(move(reader));
+  }
+
+  bool aggreg_result = true;
+  for (auto& fut : futures)
+    aggreg_result = aggreg_result && fut.get();
+
+  return aggreg_result;
+}
+}
+
+TEST_CASE("Single sized queue","[queue]") {
+  Queue<Movable> queue { 1u };
+
+  REQUIRE(queue.size() == 0);
+
+  SECTION("Simple push/pop") {
+   Movable a;
+   queue.push(std::move(a));
+   REQUIRE(1 == queue.size()); 
+   REQUIRE(!a.is_valid());
+
+   Movable b = queue.pop();
+   REQUIRE(0 == queue.size());
+   REQUIRE(b.is_valid());
+  }
+
+  SECTION("One writer one reader") {
+    REQUIRE(run_writers_readers(queue,1,1000,1));
+  }
+
+  SECTION("Many writers one reader") {
+    REQUIRE(run_writers_readers(queue,10,1000,1));
+  }
+
+  SECTION("One writer many readers") {
+    REQUIRE(run_writers_readers(queue,1,1000,10));
+  }
+
 }
 
