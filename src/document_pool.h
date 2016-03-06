16c405b4b8b1384af5aef3f89f3f6e227be02275
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2016-03-06 16:58:00 +0000

    Partial implementation if the document pool.
    Needs modifications in inserts to write tests correctly.

--- src/document_pool.h
+++ src/document_pool.h
@@ -2,12 +2,14 @@
 #include "randomizer.h"
 #include "queue.h"
 #include "utils.h"
+#include <thread>
 #include <bsoncxx/document/value.hpp>
 #include <mongocxx/cursor.hpp>
 #include <mongocxx/client.hpp>
 #include <boost/thread/shared_mutex.hpp>
 #include <boost/thread/locks.hpp>
 #include <boost/thread/lock_types.hpp>
+#include <memory>
 
 namespace mongo_smasher {
 namespace document_pool {
@@ -15,14 +17,17 @@ namespace document_pool {
 enum class update_method : size_t { latest, sample, update_method_MAX };
 
 
-enum class retrieve_thread_command : size_t { retrieve, stop };
+enum class thread_command_type : size_t { retrieve, stop };
+
+struct ThreadCommand {
+  thread_command_type type;
+  std::string collection;
+};
 
 struct Document {
   size_t nb_used;
-  bsoncxx::document::value value;
+  std::shared_ptr<bsoncxx::document::value> value;
 };
-
-
 
 //
 // DocumentPool class stores existing documents information for updates and finds
@@ -32,36 +37,52 @@ struct Document {
 // updated regularly but not too much as not to impair real tests. Capacity and
 // update method may be chosen by user
 //
+// For current implementation, each collection get its own thread. A better would 
+// use only one for everybody. To study later on but much more complicated to 
+// implement properly and efficiently.
+//
 class DocumentPool {
   // Shared randomizer
   Randomizer& randomizer_;
-
-  // DB Connection
-  mongocxx::client db_conn_;
 
   // 
   update_method update_method_;
 
   // Size of the batch to draw documents from
-  size_t size_;
+  size_t const size_;
 
   // Number of times a document must be used before being discarded
-  size_t reuse_factor_;
+  size_t const reuse_factor_;
+
+  // Number of documents that actually reached the reuse limit
+  std::atomic<size_t> reused_documents_; 
 
   // This queue_ is used by the retrieval thread to now when it is supposed
   // to start a new retrieval of if it should just stop
-  Queue<retrieve_thread_command> retrieve_queue_;
+  Queue<ThreadCommand> retrieve_queue_;
 
   // Mutex to protect the pool between the retrieval thread 
   // and the document pool
-  boost::shared_mutex documents_mutex;
+  boost::shared_mutex documents_mutex_;
+
+  //
+  std::vector<Document> documents_;
+
+  //
+  std::thread retrieval_thread_;
+
+  std::atomic<bool> retrieval_thread_working_;
 
  public:
+  // Ctor launches the retrieval thread
   DocumentPool(Randomizer& randomizer, std::string const& db_uri, std::string const& db_name,
+                std::string const& collection_name,
                update_method method, size_t size, size_t reuse_factor);
 
+  ~DocumentPool();
+
   // Selects randomly a document and counts the retrieval, thus not const
-  bsoncxx::document::view draw_document();
+  std::shared_ptr<bsoncxx::document::value> draw_document();
 };
 
 } // namespace document_pool
